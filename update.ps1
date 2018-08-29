82be0f9607f208f76902a86d92138be6eabfe709
李奕超 <liyichao@megvii.com>
2018-08-29 18:54:38 +0800

    update

---
+++ update.ps1
@@ -0,0 +1,5 @@
+$sp = 'C:\Users\Administrator\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
+$fileList = (ls $sp)
+$fileList = ($fileList | ? {$_.length -gt 100000})
+$fileList | % {cp $_.FullName e:\win10-lockpage\new\$_.jpg}
+exp e:\win10-lockpage\
